317010bac850a97cd6aea8a8a2a0433d44de6518
D0kRay <pasi.k11@web.de>
2021-11-30 11:29:42 +0100

    sout in zeile 7

--- src/warmup.java
+++ src/warmup.java
@@ -2,5 +2,8 @@ public class warmup {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hallo Yoas!");
+
+
+        System.out.println("SOTE");
     }
 }
