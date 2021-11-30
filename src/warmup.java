faaabf9e579665e159999055869eb0641353b6a6
D0kRay <pasi.k11@web.de>
2021-11-30 11:32:23 +0100

    test

--- src/warmup.java
+++ src/warmup.java
@@ -1,9 +1,15 @@
 public class warmup {
     public static void main(String[] args) {
+        int x = 0;
         System.out.println("hello world");
         System.out.println("hallo Yoas!");
 
 
         System.out.println("SOTE");
+
+
+        if(x == 0){
+            System.out.println("x = " + x);
+        }
     }
 }
